cec6047d549746e88187aaf5a62e9fcdb997f96c
Anushan Santhirakumar <73377435+Anushan1508@users.noreply.github.com>
2022-03-22 10:41:56 +0530

    server setup

--- server.js
+++ server.js
@@ -1,10 +1,18 @@
 const express = require("express");
+const dotenv = require('dotenv');
+const morgan = require('morgan');
 const app = express();
+
+dotenv.config({path:'config.env'})
+const PORT = process.env.PORT || 8080
+
+//log Requests
+app.use(morgan('tiny'));
 
 app.get('/',(req,res) => {
     res.send("rs-admin-api");
 })
 
-app.listen(4000,()=>{
-    console.log(`Server is running on http://localhost:${4000}`);
+app.listen(PORT,()=>{
+    console.log(`Server is running on http://localhost:${PORT}`);
 })
