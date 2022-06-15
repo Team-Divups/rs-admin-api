584e0023236c157450192586a82f2c6ca812220f
Anushan Santhirakumar <73377435+Anushan1508@users.noreply.github.com>
2022-06-16 01:36:12 +0530

    Login and Register & User Auth finished

--- server.js
+++ server.js
@@ -1,33 +1,59 @@
 const express = require("express");
-const dotenv = require('dotenv');
-const morgan = require('morgan');
+const cors = require("cors");
+
 const app = express();
-const bodyparser = require('body-parser');
 
+var corsOptions = {
+  origin: "http://localhost:8081"
+};
 
-dotenv.config({path:'config.env'})
-const PORT = process.env.PORT || 8080
+app.use(cors(corsOptions));
 
-//log Requests
-app.use(morgan('tiny'));
-
-//parse request to body parser
-app.use(bodyparser.urlencoded({extended: true}));
-
-app.get('/',(req,res) => {
-    res.send("rs-admin-api");
-})
-
+// parse requests of content-type - application/json
 app.use(express.json());
-const adminRouter = require('./server/routes/admin');
-app.use('/admin',adminRouter);
 
-const roleRouter = require('./server/routes/role');
-app.use('/role',roleRouter);
+// parse requests of content-type - application/x-www-form-urlencoded
+app.use(express.urlencoded({ extended: true }));
 
-const subscriptionRouter = require('./server/routes/subscription');
-app.use('/subscription',subscriptionRouter);
+// database
+const db = require("./app/models");
+const Role = db.role;
 
-app.listen(PORT,()=>{
-    console.log(`Server is running on http://localhost:${PORT}`);
-})
+db.sequelize.sync();
+// force: true will drop the table if it already exists
+// db.sequelize.sync({force: true}).then(() => {
+//   console.log('Drop and Resync Database with { force: true }');
+//   initial();
+// });
+
+// simple route
+app.get("/", (req, res) => {
+  res.json({ message: "Welcome to bezkoder application." });
+});
+
+// routes
+require('./app/routes/auth.routes')(app);
+require('./app/routes/user.routes')(app);
+
+// set port, listen for requests
+const PORT = process.env.PORT || 8080;
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}.`);
+});
+
+function initial() {
+  Role.create({
+    id: 1,
+    name: "user"
+  });
+ 
+  Role.create({
+    id: 2,
+    name: "moderator"
+  });
+ 
+  Role.create({
+    id: 3,
+    name: "admin"
+  });
+}
